5ba079c46b323017d1315b3f10c63c4a3d8bce48
DanielHabenicht <daniel-habenicht@outlook.de>
2026-08-08 18:23:36 +0200

    fix build error

--- src/sync/raw_clock.rs
+++ src/sync/raw_clock.rs
@@ -144,9 +144,8 @@ impl Clock for DefaultClock {
         }
         // This cast is necessary to make builds on musl targets work
         #[allow(clippy::unnecessary_cast)]
-        (ts.tv_sec as i64)
-            * 1_000_000
-            + (ts.tv_nsec as i64) / 1_000
+        let time = (ts.tv_sec as i64) * 1_000_000 + (ts.tv_nsec as i64) / 1_000;
+        return time;
     }
 
     #[cfg(not(target_os = "linux"))]
